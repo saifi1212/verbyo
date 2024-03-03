4b1f76cf0b8c1ae2c632abebb2de15447ae240be
saif0307 <saif.work.1077@gmail.com>
2024-03-03 17:15:46 +0500

    please

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Advertising from "@/app/components/landingPage/Advertising";
-import Hero from "@/app/components/landingPage/Hero";
-import Working from "@/app/components/landingPage/Working";
-import OrganicDistribution from "./components/landingPage/OrganicDistribution";
-import KeyBenefits from "./components/landingPage/KeyBenefits";
-import FreeAdvertising from "./components/landingPage/FreeAdvertising";
+import Advertising from "@/app/components/landing-page/Advertising";
+import Hero from "@/app/components/landing-page/Hero";
+import Working from "@/app/components/landing-page/Working";
+import OrganicDistribution from "./components/landing-page/OrganicDistribution";
+import KeyBenefits from "./components/landing-page/KeyBenefits";
+import FreeAdvertising from "./components/landing-page/FreeAdvertising";
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 
